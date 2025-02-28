f6da61a2d72705f2f49ad972b91ceb4f46e5437d
krisnawa00 <mustafacetin1619@gmail.com>
2025-02-28 14:54:15 +0200

    Initial: starting seminar3 and enum for posttype

---
+++ src/model/Post.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.Date;
+
+public class Post {
+	private String msg;
+	private Date date;
+	private int countOfLikes;
+	
+	
+	public String getMsg() {
+		return msg;
+	}
+
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+
+	public int getCountOfLikes() {
+		return countOfLikes;
+	}
+}
